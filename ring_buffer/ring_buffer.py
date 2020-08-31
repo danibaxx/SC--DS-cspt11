531420e6d38219f5f6b3f9cfca4fac97b91de277
danibaxx <danibaxx@hotmail.com>
2020-08-30 18:33:37 -0600

    setup ringbuffer, starting with get fx

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,26 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.value = [None] * capacity
+        self.pointer = 0
 
     def append(self, item):
         pass
 
     def get(self):
-        pass
+        # set empty list to variable
+        items = []
+        # return all elements in given order
+        # should not return None
+
+        #return items
+
+buffer = RingBuffer(3)
+
+buffer.get() # should return empty
+
+# buffer.append()
+# buffer.append()
+# buffer.append()
+
+buffer.get() # should return 3 values
